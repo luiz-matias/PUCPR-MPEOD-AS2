ff2f47cc69d0aa223e76b45f51df17370b82c173
Luiz Matias <luiz.matias1999@gmail.com>
2022-11-21 16:43:14 -0300

    Implemented Bubble Sort

---
+++ MPEOD-SortingAlgorithms/src/SortingAlgorithm.java
@@ -0,0 +1,13 @@
+/**
+ * Interface contract for sorting algorithms
+ */
+public interface SortingAlgorithm {
+
+    /**
+     * Sorts the array in ascending order
+     *
+     * @param array array to be sorted
+     */
+    void sort(int[] array);
+
+}
